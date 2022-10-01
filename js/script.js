2c90a3eaeee2cb16ba8499fb77ef22ccd5b31a63
danydifilippo <danydifilippo@gmail.com>
2022-10-01 22:20:26 +0200

    final changes

--- js/script.js
+++ js/script.js
@@ -1,40 +1,46 @@
+//Creo un array di emoticon per le carte
 let arrayCards = ['🍭','🍔','🔮','🎈','💍','🌂','💎',
 '💜','🍭','🍔','🔮','🎈','💍','🌂','💎','💜']
 
-// creiamo una funzione che generi le carte in maniera random e che le inserisca in array
+// creo una funzione che generi le carte in maniera random e che le inserisca in array - 
+// funzione shuffle che mescola le carte pescandone 1 alla volta con un numero a caso nell'array 
+// e le mette in ordine di pescaggio in un altro array
 function lancia(a) {
+    //vabiabile che indica lunghezza del parametro a
     let currentIndex = a.length;
+    //variabile per indice random e per valore temporaneo per ogni carta
     let temporaryValue, randomIndex;
-  
+    //finchè l'indice corrente è diverso da zero viene preso un numero random per il numero di carte rimaste
+    // diminuendone questo numero ad ogni giro, 
     while (currentIndex !== 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
+      // il numero che esce dal random viene assegnato al numero nell'array di tuttii simboli che viene inserito
+      // in ordine di pescaggio in un altro array dando così il nuovo valore alla carta.
       temporaryValue = a[currentIndex];
       a[currentIndex] = a[randomIndex];
       a[randomIndex] = temporaryValue;
     }
+    // viene creato un nuovo ordine di carte
     return a;
   }
 
-// creiamo una variabile che ripulisce il tempo ad ogni inizio gioco 
-  let interval
+// creo una variabile che utilizzerò per ripulire il tempo ad ogni inizio gioco 
+  let interval;
   
-// creiamo un timer di 2 minuti per trovare tutte le carte
-
+// creo un timer di 1 minuto per trovare tutte le carte
 
 function timerStart () {
-    let s=30, m=1;
+    let s=60;
     let timer=document.getElementById('timer');
     interval = setInterval (function() {
-        timer.innerHTML = `Hai a disposizione: ${m} min ${s} sec per trovare tutte le carte`;
+        timer.innerHTML = `Hai a disposizione: ${s} secondi per trovare tutte le carte`;
         s--;
-    if(s==0 && m===1){
-        clearInterval(interval);
-        let finishLose = document.getElementById('finishLose')
-        finishLose.classList.add('attiva');
-        }else{
-        m--;
-        s=59;
+        if(s<0){
+            // appena finisce il tempo si attiva la classe al messaggio di 'ritenta' e pulisce l'intervallo
+            clearInterval(interval);
+            let lose = document.getElementById('lose')
+            lose.classList.add('attiva');
         }
     },1000);
 }
@@ -42,16 +48,21 @@ function timerStart () {
 // button start che fa partire il gioco
 
 function start() {
+    //mescolare le carte con funzione shuffle
     let arraylancia = lancia(arrayCards);
-
+    //far ripartire il tempo da un minuto
     clearInterval(interval);
+    // richiamo arrayComparison vuoto che mi servirà da contenitore per comparare le prime due carte pescate
     arrayComparison = [];
-
+    // creo una variabile per avere una lista delle carte
     let lista = document.getElementById('grid');
+        // finchè il parent lista ha dei figli ad ogni giro viene rimosso il primo figlio
         while (lista.hasChildNodes()) {
             lista.removeChild(lista.firstChild);
         }
-
+        //ciclo for che parte da zero fino ad massimo di 16 carte, crea un div per ogni carta e per ogni simbolo
+        // dare la classe icon per il div del simbolo che sarà il figlio del div carta che sarà figlio del div griglia
+        // i div dei simboli saranno elementi dell'array per la funzione shuffle
         for (i=0;i<16;i++) {
             let box = document.createElement('div');
             let element = document.createElement('div');
@@ -60,11 +71,13 @@ function start() {
             element.innerHTML = arraylancia[i];
         }
 
+        //parte funzione tempo
     timerStart();
-
+        // dare variabile a tutte le carte con classe icon e icons  che mette tutte in un array
     let icon = document.getElementsByClassName('icon');
     let icons = [...icon];
-
+        // ciclo for per tutta la lunghezza dell'array che contiene le icone, parte da 0 e va avanti
+        // applica la funzione display e openFinish per le carte cliccate
     for (i=0; i<icons.length;i++){
         icons[i].addEventListener('click',display);
         icons[i].addEventListener('click',openFinish);
@@ -89,7 +102,7 @@ function display(eventclick){
                 item.classList.add('find','disability');
             });
         // pulisce l'arrayComparison
-            arrayComparison=[];
+        arrayComparison=[];
         } else {
         // se non uguali aggiunge la classe disability per disattivare il click
             icons.forEach(function(oggetto) {
@@ -108,17 +121,16 @@ function display(eventclick){
                     }
                 });
         // pulisce l'arrayComparison e termina la funzione in 0.7 secondi.
-                arrayComparison=[];
+        arrayComparison=[];
             },700);
         }
     }
 }
 
-// funzione per la finestra finale, se vengono indovinate tutte le carte
+// funzioni per le finestre finali: 
 let finish = document.getElementById('finish');
 
-
-
+//se vengono indovinate tutte le carte viene inserita la classe attiva alla finestra
 function openFinish(){
     let find = document.getElementsByClassName('find');
     if(find.length>=16){
@@ -127,17 +139,17 @@ function openFinish(){
         closeFinish();
     }
 }
-
+// viene rimossa la classe attiva se si chiude la finestra
 function closeFinish(){
     closeicon.addEventListener('click', function(n){
         finish.classList.remove('attiva');
-        finishLose.classList.remove('attiva');
+        lose.classList.remove('attiva');
         start();
     });
 }
-
+//funzione gioca di nuovo e ritenta
 function playAgain(){
-    finish.classList.remove('attiva')
-    finishLose.classList.remove('attiva');
+    finish.classList.remove('attiva');
+    lose.classList.remove('attiva');
     start();
 }
